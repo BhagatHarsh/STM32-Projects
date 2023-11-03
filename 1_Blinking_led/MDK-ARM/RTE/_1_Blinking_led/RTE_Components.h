9d14fad1f253c5fa1e1b28421dd66abff1f49bc9
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-11-03 14:46:30 +0530

    init

---
+++ 1_Blinking_led/MDK-ARM/RTE/_1_Blinking_led/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '1_Blinking_led' 
+ * Target:  '1_Blinking_led' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */

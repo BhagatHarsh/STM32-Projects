0439dd03f2d809e7beec52d5671b6917a601c478
Kaival <mailkaivalshah@gmail.com>
2023-11-24 11:31:06 +0530

    Added the final code

---
+++ kingk_ultra_pro_max/MDK-ARM/RTE/_kingk_ultra_pro_max/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'kingk_ultra_pro_max' 
+ * Target:  'kingk_ultra_pro_max' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
